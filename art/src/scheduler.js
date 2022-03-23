693d1ddc6972dae8426b060bfcf1f97274c812ed
sullayang(杨金伟) <sullayang@futunn.com>
2022-03-23 17:46:09 +0800

    feat: 修改任务调度

--- art/src/scheduler.js
+++ art/src/scheduler.js
@@ -5,8 +5,7 @@ export const taskList = new TaskList();
 // requestIdleCallback的fps为20，所以deadline。timeRemaining()范围为0-50ms。
 function workLoop(deadline) {
   // 如果存在闲置时间或者有任务超出最后执行时间,则取出任务执行
-  let firstTimeOut = taskList.getFirstTimeOut();
-  while (deadline.timeRemaining() > 1 || (firstTimeOut && firstTimeOut <= Date.now())) {
+  while (deadline.timeRemaining() > 1 || (taskList.getFirstTimeOut() && taskList.getFirstTimeOut() <= Date.now())) {
     let task = taskList.shift();
     if (task) {
       task.val();
