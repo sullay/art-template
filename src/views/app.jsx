aae2fd5c3396df9d6445fdf5ebad9dcd13b2dd90
sullayang(杨金伟) <sullayang@futunn.com>
2022-04-01 17:00:57 +0800

    测试

--- src/views/app.jsx
+++ src/views/app.jsx
@@ -6,20 +6,23 @@ class App extends art.Component {
     super(node);
     this.data = {
       hasTitle: true,
-      num: 1
+      count: 1
     }
     this.start;
   }
   addNum = () => {
-    if(this.data.num ===1)  this.start = performance.now();
-    if (this.data.num >= 50000) {
+    if(this.data.count ===1)  this.start = performance.now();
+    if (this.data.count >= 50000) {
       console.log(performance.now() - this.start);
       console.log('完成');
       return;
     }
-    this.setDataNow({ num: this.data.num + 1 }, () => {
+    this.setData({ count: this.data.count + 1 }, () => {
       this.addNum();
     })
+  }
+  xxx = ()=>{
+    this.setData({ hasTitle: !this.data.hasTitle })
   }
   render() {
     return (
@@ -27,7 +30,7 @@ class App extends art.Component {
         <Test count={this.data.count} hasTitle={this.data.hasTitle}/>
         <button onclick={this.addNum}>+1</button>
         <br/>
-        <button onclick={() => this.setData({ hasTitle: !this.data.hasTitle })}>确认</button>
+        <button onclick={this.xxx}>确认</button>
       </div>
     )
   }
